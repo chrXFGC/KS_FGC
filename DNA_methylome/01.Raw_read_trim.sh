4f9a6648a7f17fd1894c205cac62d0ee13adf2ea
FGC <165400981+chrXFGC@users.noreply.github.com>
2024-06-13 19:32:56 +0800

    Update 01.Raw_read_trim.sh

--- DNA_methylome/01.Raw_read_trim.sh
+++ DNA_methylome/01.Raw_read_trim.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Input Arguments
 fq1=$1
 fq2=$2
 cln_dir=$3
 
+# Run Trim Galore 
 /trim_galore --path_to_cutadapt /cutadapt --clip_R1 9 --clip_R2 9 --paired --quality 20 --phred33 --stringency 3 --gzip --length 50  --trim1 --paired $fq1 $fq2 --output_dir $cln_dir 
 
