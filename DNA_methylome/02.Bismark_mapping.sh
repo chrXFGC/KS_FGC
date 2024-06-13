d7fdb859326d6e6251f6a46a907ddd46128991ed
FGC <165400981+chrXFGC@users.noreply.github.com>
2024-06-13 19:34:37 +0800

    Update 02.Bismark_mapping.sh

--- DNA_methylome/02.Bismark_mapping.sh
+++ DNA_methylome/02.Bismark_mapping.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Input Arguments
 sample=$1
 cln_fq1=$2
 cln_fq2=$3
-ref=/hg19/Bismark2
+ref=/hg19/Bismark2  # Reference genome 
 
+# reads mapping  with bismark
 /Bismark-0.22.3/bismark   --multicore 4   --score_min L,0,-0.6  -quiet --non_directional -o /outdir/$sample  $ref  $cln_fq1
 /Bismark-0.22.3/bismark   --multicore 4   --score_min L,0,-0.6  -quiet --non_directional -o /outdir/$sample  $ref  $cln_fq2
 
