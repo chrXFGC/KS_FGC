3d91450831785a3781c26dd6ad9c0fe6f6ab9447
FGC <165400981+chrXFGC@users.noreply.github.com>
2024-06-13 19:30:05 +0800

    Update 02.Tophat_mapping.sh

--- Smart-seq2/02.Tophat_mapping.sh
+++ Smart-seq2/02.Tophat_mapping.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 samp_name=$1
 brief_name=$2  #with modify the sample names 
 cln_dir=/clean_data/RNA
 tophat_dir=/mapping/RNA/hg19
-genome=/Data_Base/smrt-seq2/hg19/hg19_ERCC92_RGC
-gtf_file=/Data_Base/smrt-seq2/hg19/refGene.gtf
+genome=/Data_Base/smrt-seq2/hg19/hg19_ERCC92_RGC # Genome reference
+gtf_file=/Data_Base/smrt-seq2/hg19/refGene.gtf # GTF file 
 
+# Mapping RNA-seq data with Tophat
 tophat   -p 8 -G $gtf_file --library-type fr-unstranded                   \
    -o $tophat_dir/$brief_name  $genome                              \
    $cln_dir/$samp_name/$samp_name.R1.clean.fq.gz
